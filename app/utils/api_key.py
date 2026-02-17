7b8c5f21efdc864bf1bfa0d5a3d8cabc7f1fa361
785943 <142229367+785943@users.noreply.github.com>
2026-02-18 02:03:30 +0800

    Update api_key.py

--- app/utils/api_key.py
+++ app/utils/api_key.py
@@ -84,7 +84,7 @@ async def test_api_key(api_key: str) -> bool:
     try:
         import httpx
 
-        url = "http://127.0.0.1:8045/v1beta/models?key={}".format(
+        url = "https://generativelanguage.googleapis.com/v1beta/models?key={}".format(
             api_key
         )
         async with httpx.AsyncClient() as client:
